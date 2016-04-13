c49011b19c4c7b5c932cb3d102fa44810d72edb7
ogunleye <o.d.ogunleye@rgu.ac.uk>
2016-04-13 14:40:50 +0100

    work

--- PhotoComment/connection.php
+++ PhotoComment/connection.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
-define('DB_USERNAME', 'b99256e599d9fc');
-define('DB_PASSWORD', '64c23a04');
+define('DB_SERVER', 'us-cdbr-azure-west-c.cloudapp.net');
+define('DB_USERNAME', 'b6934f47b40a4f');
+define('DB_PASSWORD', 'c5573120');
 define('DB_DATABASE', 'rgutest');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
